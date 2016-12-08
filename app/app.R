bab0ffd4617b76392c6b9d5e75c4d3e52163f076
Daniel Kirsch <danishkirel@gmail.com>
2016-12-08 15:39:08 +0100

    Set fixed colours for TS

--- app/app.R
+++ app/app.R
@@ -68,10 +68,20 @@ units$updated = T # only updated unites need to be redrawn (true for initial ren
 units$hovered = F
 
 # Scales
-cfac = colorFactor(rainbow(length(entity_ids)), levels=sample(entity_ids))
+# cfac = colorFactor(rainbow(length(entity_ids)), levels=sample(entity_ids))
+colors = brewer.pal(8, 'Set1') # show_col(brewer.pal(9,'Set1'))
+valid_colors = colors[2:8]
+warning_color =  colors[1]
+entity_ids_color = c(
+  "07G16", "07G02", "07G01", "07G21", "07G14", "07G07", "07G15", "07G03", "07G13", "07G12", "07G10", "07G17", "07G06", "07G18",
+  "07G19", "07G24", "07G05", "07G22", "07G23", "07G25", "07G20", "07G36", "07G27", "07G37", "07G35", "07G30", "07G28", "07G32",
+  "07G29", "07G34", "07G26", "07G31"
+)
+palette = rep(valid_colors, length(entity_ids_color)/length(valid_colors)+1)[1:length(entity_ids_color)]
+cfac = colorFactor(palette, levels=entity_ids_color)
 entity_colors = function(entity_id, desaturate) {
   color = cfac(entity_id)
-  ifelse(desaturate, desat(color, 0.2), color)
+  ifelse(desaturate, desat(color, 0.3), color)
 }
 
 desat <- function(cols, sat=0.5) {
--- app/deps.R
+++ app/deps.R
@@ -1,4 +1,4 @@
-requiredPackages = c('rgdal', 'readr', 'dplyr', 'tidyr', 'leaflet', 'htmltools', 'purrr', 'colorspace', 'ggplot2', 'shiny', 'DT', 'memoise', 'future', 'futile.logger')
+requiredPackages = c('rgdal', 'readr', 'dplyr', 'tidyr', 'leaflet', 'htmltools', 'purrr', 'colorspace', 'RColorBrewer', 'ggplot2', 'shiny', 'DT', 'memoise', 'future', 'futile.logger')
 for(p in requiredPackages){
   if(!require(p, character.only = TRUE)) install.packages(p, repos='http://cran.r-project.org')
   library(p, character.only = TRUE)
