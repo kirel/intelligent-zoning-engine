eb16ea1d31fabf782cdbd371cfded750d1ddeef7
noashin <noa.shinitski@gmail.com>
2016-12-15 17:29:15 +0100

    clean and comment

--- ga_py_2/ga_2.py
+++ ga_py_2/ga_2.py
@@ -5,8 +5,23 @@ from read_meta_data_2 import read_data
 
 units, entities, weights, capacity, units_population = read_data('../app-data', 'max')
 
+OVER_CAPACITY_PENALTY = 1
+UNDER_CAPACITY_PENALTY = 1
+
+DIST_WEIGHT = 1 / 2000 ** 2
+OVER_CAPACITY_WEIGHT = 1 / 200
+UNDER_CAPACITY_WEIGHT = 1 / 200
+
 
 def clone_population(population):
+    """This function performs deep copy of a population
+
+    Args:
+        population:
+
+    Returns: copied population
+
+    """
     new_population = []
     for individual in population:
         new_population.append(np.copy(individual))
@@ -15,17 +30,16 @@ def clone_population(population):
 
 
 def initialize_individual(num_entities, num_units):
-    """
+    """This function initialize a random assignment
 
     Args:
         num_entities:
         num_units:
 
-    Returns:
+    Returns: random assignment
 
     """
     assigns = np.random.randint(0, num_entities, num_units)
-
     assign_mat = np.zeros((num_units, num_entities))
     assign_mat[np.arange(num_units), assigns] = 1.
 
@@ -33,27 +47,23 @@ def initialize_individual(num_entities, num_units):
 
 
 def fitness(assignment):
-    """ Fitness function to be minimized
+    """ Fitness function to be minimized.
+        The fitness is quadratic in the distance between the units and entities,
+        and in over and under capacity. the distance and capacity may have different weights.
 
     Args:
         assignment: assignment of each unit to an entity.
                     sparse matrix with one 1 entry in each row
-        weights:
 
     Returns: the fitness of the individual (represented by A)
 
     """
 
-    OVER_CAPACITY_PENALTY = 1
-    UNDER_CAPACITY_PENALTY = 1
-
-    DIST_WEIGHT = 1 / 2000 ** 2
-    OVER_CAPACITY_WEIGHT = 1 / 200
-    UNDER_CAPACITY_WEIGHT = 1 / 200
-
+    # difference between capacity and assignment results
     assignment_population = np.dot(assignment.T, units_population)
     assign_capacity_diff = capacity - assignment_population
 
+    # handle differently over and under capacity
     over_cap_vec = (assign_capacity_diff * (assign_capacity_diff > 0) * OVER_CAPACITY_PENALTY) ** 2
     under_cap_vec = (assign_capacity_diff * (assign_capacity_diff < 0) * UNDER_CAPACITY_PENALTY) ** 2
 
@@ -64,18 +74,31 @@ def fitness(assignment):
 
 
 def mutation(population, sampled_population_ind, mutation_frac, hueristic_exponent):
+    """This function mutates individuals in the population.
+
+    Args:
+        population:
+        sampled_population_ind: indices of individuals in the populations to be mutated.
+        mutation_frac: fraction of units in the individual to be nutated
+        hueristic_exponent:
+
+    Returns: a population of mutated individuals
+
+    """
     if mutation_frac == 0:
         return population
+
     num_units, num_entities = population[0].shape
     num_mutations = int(mutation_frac * num_units)
 
     for ind in sampled_population_ind:
+        # calculate mutation probability for each unit.
+        # hueristic_exponen allows mutating with high prob units with large distances.
         fitness_vals_normed = np.multiply(population[ind], weights).sum(axis=1) / np.max(weights)
         exploration_fitness_vals = fitness_vals_normed ** hueristic_exponent
         mutation_prob = exploration_fitness_vals / np.sum(exploration_fitness_vals)
 
         # chose which rows to mutate based on the mutation probability
-
         mutated_units = np.random.choice(np.arange(num_units), num_mutations, False, mutation_prob)
         new_entities = np.random.randint(0, num_entities, len(mutated_units))
         population[ind][mutated_units, :] = 0.
@@ -85,21 +108,31 @@ def mutation(population, sampled_population_ind, mutation_frac, hueristic_expone
 
 
 def crossover(ind_a, ind_b):
+    """This function mates between two individuals and creates two children using swap strategie.
+
+    Args:
+        ind_a:
+        ind_b:
+
+    Returns: 2 children
+
+    """
+
     diffs = np.logical_not(np.equal(ind_a, ind_b).prod(axis=1))
     num_diffs = np.sum(diffs)
 
+    # don't swap if the assignment of the individuals is identical
     if num_diffs == 0:
-        # print('a')
         return []
 
     child_a = np.copy(ind_a)
     child_b = np.copy(ind_b)
 
+    # chose randomly which units to swap
     fraction = np.random.rand(1)
-
     num_genes = int(fraction * num_diffs)
-    # import ipdb; ipdb.set_trace()
     swap_ind = np.random.choice(np.arange(ind_a.shape[0])[diffs], num_genes)
+
     child_a[swap_ind] = ind_b[swap_ind]
     child_b[swap_ind] = ind_a[swap_ind]
 
@@ -107,6 +140,15 @@ def crossover(ind_a, ind_b):
 
 
 def get_prob_from_fitness(population):
+    """This function calculates probabilities to be mutated or mated based on the
+    individuals' fitness
+
+    Args:
+        population:
+
+    Returns: probabilities
+
+    """
     fitness_vals = np.array([fitness(population[ind]) for ind in range(len(population))])
     fitness_vals = -1. * fitness_vals + min(fitness_vals) + max(fitness_vals)
     probs = fitness_vals / sum(fitness_vals)
@@ -115,8 +157,20 @@ def get_prob_from_fitness(population):
 
 
 def breed(population, hueristic_exponent, mutation_frac, num_mutants=100, num_pairs=50):
-    # import ipdb; ipdb.set_trace()
-    # calculate probability to chose each individual in the population
+    """This function breeds the current population to generate the new population.
+    The new population consists of the old population, the mutations and the children.
+
+    Args:
+        population:
+        hueristic_exponent: used to determine the probability of a unit to be swaped.
+        mutation_frac: what fraction of units in an individual should be mutated
+        num_mutants: number of individuals in the population to be mutated
+        num_pairs: number of pairs to be mated
+
+    Returns: new population
+
+    """
+    # calculate probability to chose each individual in the population for mutation
     probs = get_prob_from_fitness(population)
 
     sampled_population_ind = np.random.choice(np.arange(len(population)), num_mutants, True, probs)
@@ -124,8 +178,9 @@ def breed(population, hueristic_exponent, mutation_frac, num_mutants=100, num_pa
                                   hueristic_exponent)
 
     mating_population = clone_population(population) + clone_population(mutated_population)
+
+    # calculate probability to chose each individual in the mating population for mating
     probs_mating = get_prob_from_fitness(mating_population)
-    # import ipdb; ipdb.set_trace()
     pairs = [np.random.choice(np.arange(len(mating_population)), 2, False, probs_mating) for i in range(num_pairs)]
 
     mated = []
@@ -140,8 +195,17 @@ def breed(population, hueristic_exponent, mutation_frac, num_mutants=100, num_pa
     return population[:] + mutated_population[:] + mated[:]
 
 
-def select(population, survival_fraction=1., max_population=50):
-    # print(len(population))
+def select(population, survival_fraction=0.5, max_population=50):
+    """This function selects the best candidates from the new population
+
+    Args:
+        population:
+        survival_fraction:
+        max_population:
+
+    Returns: the best individuals from the populaiton
+
+    """
     sorted_population = sorted(population, key=fitness)
     num_survivors = min(int(len(population) * survival_fraction), max_population)
     survivors = sorted_population[:num_survivors]
@@ -149,19 +213,38 @@ def select(population, survival_fraction=1., max_population=50):
 
 
 def optimization_step(population, num_steps):
+    """This function performs one optimization step - breeds new population,
+    select the best candidates from it and writes the best individual to the data base.
+
+    Args:
+        population:
+        num_steps: current number of optimization steps.
+
+    Returns:
+
+    """
     hueristic_exponent = 20. / num_steps
     mutation_frac = max(0.1, 1. - num_steps / 3.)
 
     new_population = breed(population[:], hueristic_exponent, mutation_frac)
     survivors = select(new_population)
 
-    # best_solution = new_population[0]
-    # write_results(best_solution)
+    best_solution = new_population[0]
+    write_results(best_solution)
 
     return survivors
 
 
 def write_results(best_solution):
+    """This function matches between entities and units ids based on
+    the asignment matrix and writes it to the data base.
+
+    Args:
+        best_solution:
+
+    Returns:
+
+    """
     assigned_entities = []
     for assign in best_solution:
         ent_idx = np.where(assign > 0)[0]
@@ -175,28 +258,5 @@ def write_results(best_solution):
 
     df = pd.DataFrame(columns=['units', 'entities'], data=assignment)
 
+    # TODO: write result to data base!!
     return
-
-
-if __name__ == '__main__':
-    num_units = len(units)
-    num_entities = len(entities)
-
-    population_num = 100
-
-    init_pop = [initialize_individual(num_entities, num_units) for i in range(population_num)]
-    pop = init_pop[:]
-    fitness_res = []
-    num_steps = 1
-    for i in range(50):
-        new_pop = optimization_step(pop[:], num_steps)
-
-        best_solution = new_pop[0]
-        fitness_res.append(fitness(best_solution))
-        pop = new_pop[:]
-        num_steps += 1
-
-    # import ipdb;
-
-    ipdb.set_trace()
-    # return res
