1f973cc2b3aef984c6fdcac04d240bdc8ad4bf4b
noashin <noa.shinitski@gmail.com>
2016-12-15 16:46:33 +0100

    read also capacity and population data

--- ga_py_2/read_meta_data_2.py
+++ ga_py_2/read_meta_data_2.py
@@ -3,11 +3,12 @@
  and extract the units and entities names and the weights.
 """
 
+import os
 import numpy as np
 import pandas as pd
 
 
-def read_weights(path_to_weights, measurement='max'):
+def read_data(folder, measurement='max'):
     """ This function reads the weights file and extracts the necessary information
 
     Args:
@@ -19,7 +20,9 @@ def read_weights(path_to_weights, measurement='max'):
              and a weight matrix (of measurements) with a unit in each row and entity in each column
 
     """
-    df_weights = pd.read_csv(path_to_weights)
+    df_weights = pd.read_csv(os.path.join(folder, 'weights.csv'))
+    df_entities = pd.read_csv(os.path.join(folder, 'entities.csv'))
+    df_units = pd.read_csv(os.path.join(folder, 'units.csv'))
 
     # get units and entities ids
     units_id = df_weights['unit_id'].unique()
@@ -29,4 +32,12 @@ def read_weights(path_to_weights, measurement='max'):
     weights_vec = np.array(df_weights[measurement])
     weights_mat = weights_vec.reshape(len(units_id), len(entities_id))
 
-    return units_id, entities_id, weights_mat
+    # get capacity information
+    entities_capacity = df_entities.iloc[:, 1].as_matrix()
+
+    # only use population of units that are in the weighs matrix
+    df_units = df_units[df_units['unit_id'].isin(units_id)]
+    units_population = df_units.iloc[:, 1].as_matrix()
+
+
+    return units_id, entities_id, weights_mat, entities_capacity, units_population
