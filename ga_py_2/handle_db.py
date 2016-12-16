20dcd66059fad73f81108becffceb6e7725b9914
noashin <noa.shinitski@gmail.com>
2016-12-16 15:09:59 +0100

    use pandas to write full data to db. add function to get the current assignment

--- ga_py_2/handle_db.py
+++ ga_py_2/handle_db.py
@@ -1,42 +1,33 @@
 import sqlite3
 import pandas as pd
+import numpy as np
 
 sqlite_file = 'assignments_db.sqlite'
 instructions_table_name = 'instructions'
 assign_table_name = 'assignments'
 
-instruction_id_column = 'idx'
+instruction_id_column = 'instruction'
+instruction_column = 'on_off'
 
 
 def create_db():
     init_assignment = pd.read_csv('../app-data/assignment.csv')
     init_assignment.set_index('unit_id')
 
-    f_type= 'INTEGER'
-
     conn = sqlite3.connect(sqlite_file)
 
     c = conn.cursor()
 
-    # units_col = 'units_id'
-    # units_field_type = 'INTEGER'
-    # entities_col = 'entities_id'
-    # entities_field_type = 'TEXT'
-    # c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'.
-    #          format(tn=assign_table_name, nf=units_col, ft=units_field_type))
-    # c.execute("ALTER TEBLE {tn} ADD COLUMN '{cn}' {ct}".
-    #          format(tn=assign_table_name, cn=entities_col, ct=entities_field_type))
-
-    # what is it doing as primary key?
-
-    update_assignment(init_assignment)
+    set_assignment(init_assignment)
 
     c.execute("CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)" \
-              .format(tn=instructions_table_name, nf=instruction_id_column, ft=f_type))
+              .format(tn=instructions_table_name, nf=instruction_id_column, ft='TEXT'))
     c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}".
-              format(tn=instructions_table_name, cn='optimize', ct='INTEGER'))
-    c.execute("INSERT INTO {tn} ({idf}, {cn}) VALUES (0, 0)".
-              format(tn=instructions_table_name, idf=instruction_id_column, cn='optimize'))
+              format(tn=instructions_table_name, cn=instruction_column, ct='INTEGER'))
+    c.execute("INSERT INTO {tn} ({idf}, {cn}) VALUES ('optimize', 0)".
+              format(tn=instructions_table_name, idf=instruction_id_column, cn=instruction_column))
+    c.execute("INSERT INTO {tn} ({idf}, {cn}) VALUES ('include_current', 0)".
+              format(tn=instructions_table_name, idf=instruction_id_column, cn=instruction_column))
 
     conn.commit()
     conn.close()
@@ -44,13 +35,13 @@ def create_db():
     return
 
 
-def read_instruction():
+def get_instruction(instruction):
     conn = sqlite3.connect(sqlite_file)
 
     c = conn.cursor()
 
-    c.execute('SELECT * FROM {tn} WHERE {cn}=0'.
-              format(tn=instructions_table_name, cn=instruction_id_column))
+    c.execute("SELECT * FROM {tn} WHERE {cn}={val}".
+              format(tn=instructions_table_name, cn=instruction_id_column, val=instruction))
     res = c.fetchall()
 
     conn.close()
@@ -58,7 +49,7 @@ def read_instruction():
     return res
 
 
-def update_assignment(assignment_df):
+def set_assignment(assignment_df):
     """
 
     Args:
@@ -74,3 +65,24 @@ def update_assignment(assignment_df):
     conn.close()
 
     return
+
+
+def get_current_assignment():
+
+    conn = sqlite3.connect(sqlite_file)
+    query = 'SELECT * FROM {tn}'.format(tn=assign_table_name)
+
+    assignment_df = pd.read_sql(query, conn, index_col='index')
+
+    conn.close()
+
+    #import ipdb; ipdb.set_trace()
+    entities = list(assignment_df['entity_id'].unique())
+    units = list(assignment_df['unit_id'])
+
+    assign_mat = np.zeros((len(units), len(entities)))
+
+    for i, entity in enumerate(assignment_df['entity_id']):
+        assign_mat[i, entities.index(entity)] = 1
+
+    return assign_mat
