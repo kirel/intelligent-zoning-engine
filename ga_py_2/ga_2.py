02a374211074b7fb605dc1ea7013a12ee98cac99
noashin <noa.shinitski@gmail.com>
2016-12-16 15:09:01 +0100

    take out the writing function. add function to initialize entire population

--- ga_py_2/ga_2.py
+++ ga_py_2/ga_2.py
@@ -28,8 +28,18 @@ def clone_population(population):
 
     return new_population
 
+def initialize_population(population_size):
+    num_entities = len(entities)
+    num_units = len(units)
+    population = []
 
-def initialize_individual(num_entities, num_units):
+    for i in range(population_size):
+        population.append(initialize_individual(num_units, num_entities))
+
+    return population
+
+
+def initialize_individual(num_units, num_entities):
     """This function initialize a random assignment
 
     Args:
@@ -39,6 +49,7 @@ def initialize_individual(num_entities, num_units):
     Returns: random assignment
 
     """
+
     assigns = np.random.randint(0, num_entities, num_units)
     assign_mat = np.zeros((num_units, num_entities))
     assign_mat[np.arange(num_units), assigns] = 1.
@@ -229,34 +240,7 @@ def optimization_step(population, num_steps):
     new_population = breed(population[:], hueristic_exponent, mutation_frac)
     survivors = select(new_population)
 
-    best_solution = new_population[0]
-    write_results(best_solution)
-
     return survivors
 
 
-def write_results(best_solution):
-    """This function matches between entities and units ids based on
-    the asignment matrix and writes it to the data base.
 
-    Args:
-        best_solution:
-
-    Returns:
-
-    """
-    assigned_entities = []
-    for assign in best_solution:
-        ent_idx = np.where(assign > 0)[0]
-
-        if len(ent_idx) > 1:
-            print('error')
-
-        assigned_entities.append(entities[ent_idx[0]])
-
-    assignment = np.array([units, assigned_entities]).T
-
-    df = pd.DataFrame(columns=['units', 'entities'], data=assignment)
-
-    # TODO: write result to data base!!
-    return
