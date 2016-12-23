88e23702ecb925a9da1fe5cdf028d10d6fc8fec7
noashin <noa.shinitski@gmail.com>
2016-12-23 12:46:17 +0100

    Refactor instructions table to be a queue and add input_assignment table

--- ga_py_2/handle_db.py
+++ ga_py_2/handle_db.py
@@ -2,12 +2,13 @@ import sqlite3
 import pandas as pd
 import numpy as np
 
-sqlite_file = 'assignments_db.sqlite'
-instructions_table_name = 'instructions'
-assign_table_name = 'assignments'
+sqlite_file = 'zoning_db.sqlite'
 
-instruction_id_column = 'instruction'
-instruction_column = 'on_off'
+solution_table_name = 'solution'
+input_table_name = 'input'
+
+instructions_table_name = 'instructions'
+instruction_column = 'instruction'
 
 
 def create_db():
@@ -25,15 +26,9 @@ def create_db():
     c = conn.cursor()
 
     set_assignment(init_assignment)
-
+    set_assignment(init_assignment, input_table_name)
     c.execute("CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)".
-              format(tn=instructions_table_name, nf=instruction_id_column, ft='TEXT'))
-    c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}".
-              format(tn=instructions_table_name, cn=instruction_column, ct='INTEGER'))
-    c.execute("INSERT INTO {tn} ({idf}, {cn}) VALUES ('optimize', 0)".
-              format(tn=instructions_table_name, idf=instruction_id_column, cn=instruction_column))
-    c.execute("INSERT INTO {tn} ({idf}, {cn}) VALUES ('include_current', 0)".
-              format(tn=instructions_table_name, idf=instruction_id_column, cn=instruction_column))
+              format(tn=instructions_table_name, nf=instruction_column, ft='TEXT'))
 
     conn.commit()
     conn.close()
@@ -41,30 +36,35 @@ def create_db():
     return
 
 
-def get_instruction(instruction):
-    """This function returns the current value of instruction column in the instructions table.
+def get_instruction():
+    """This function returns the first value in the table and delete it. FIFO.
 
-    Args:
-        instruction: either "'optimize'" or "'include_current'"
-
-    Returns: 0 or 1
+    Returns: instruction string
 
     """
     conn = sqlite3.connect(sqlite_file)
 
     c = conn.cursor()
 
-    c.execute("SELECT * FROM {tn} WHERE {cn}={val}".
-              format(tn=instructions_table_name, cn=instruction_id_column, val=instruction))
+    read_str = "SELECT * FROM " + instructions_table_name + " ORDER BY ROWID ASC LIMIT 1"
+    delete_str = "DELETE FROM " + instructions_table_name +  \
+                 " WHERE ROWID IN (Select ROWID from " + instructions_table_name + " ASC LIMIT 1)"
+    c.execute(read_str)
     res = c.fetchall()
 
+    if res:
+        res = res[0][0]
+        c.execute(delete_str)
+
+    conn.commit()
     conn.close()
 
     return res
 
 
-def set_assignment(assignment_df):
+def set_assignment(assignment_df, table_name=solution_table_name):
     """This functions writes assignment_df to he data base.
+    Should no be used outside this file. Should be used only to create the db.
 
     Args:
         assignment_df: expecting unit_id as index and one column 'entity_id'
@@ -74,22 +74,22 @@ def set_assignment(assignment_df):
     """
 
     conn = sqlite3.connect(sqlite_file)
-    assignment_df.to_sql(assign_table_name, conn, if_exists='replace')
+    assignment_df.to_sql(table_name, conn, if_exists='replace')
     conn.commit()
     conn.close()
 
     return
 
 
-def get_current_assignment():
-    """This function gets the current assignment (units and entities) from the data base.
+def get_input_assignment():
+    """This function gets the input assignment (units and entities) from the data base.
 
     Returns: matrix form (zeros and ones) of the current assignment in the data bsae
 
     """
 
     conn = sqlite3.connect(sqlite_file)
-    query = 'SELECT * FROM {tn}'.format(tn=assign_table_name)
+    query = 'SELECT * FROM {tn}'.format(tn=input_table_name)
 
     assignment_df = pd.read_sql(query, conn, index_col='index')
 
